e4fc55ec812e2e7ccc18181bd8b82fb88dee262d
Geoffrey Canivet <geoffrey.canivet@gmail.com>
2025-01-10 18:01:49 +0100

    Modal-FormulaireAddItemOcc

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,25 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Couleurs personnalisées
+        gris: {
+/*          light: "#63b3ed", // Version claire*/
+          DEFAULT: "#3182ce", // Couleur par défaut
+/*          dark: "#2c5282", // Version foncée*/
+        },
+        secondary: {
+          light: "#f6ad55",
+          DEFAULT: "#ed8936",
+          dark: "#c05621",
+        },
+        customGreen: "#32a852", // Couleur unique sans déclinaisons
+      },
+    },
   },
-  plugins: [],
+  plugins: ['flowbite/plugin'],
 }
